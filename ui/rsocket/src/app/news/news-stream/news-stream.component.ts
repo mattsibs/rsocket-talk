3883b40692db8a83cb09c89291c2a553b4b9920f
mattSibson <matthew.sibson@babylonhealth.com>
2020-01-31 11:00:47 +0000

    Adding styling to news items

--- ui/rsocket/src/app/news/news-stream/news-stream.component.ts
+++ ui/rsocket/src/app/news/news-stream/news-stream.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {NewsItem} from "../news.interface";
 import {NewsService} from "../news.service";
 
+
 @Component({
   selector: 'app-news-stream',
   templateUrl: './news-stream.component.html',
@@ -18,7 +19,34 @@ export class NewsStreamComponent implements OnInit {
       content: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.\n' +
         ' A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally\n' +
         'bred for hunting.'
-    }
+    },
+    {
+      id: '1',
+      title: 'Shiba Inu',
+      subtitle: 'Dog Breed',
+      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      content: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.\n' +
+        ' A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally\n' +
+        'bred for hunting.'
+    },
+    {
+      id: '1',
+      title: 'Shiba Inu',
+      subtitle: 'Dog Breed',
+      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      content: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.\n' +
+        ' A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally\n' +
+        'bred for hunting.'
+    },
+    {
+      id: '1',
+      title: 'Shiba Inu',
+      subtitle: 'Dog Breed',
+      image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      content: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.\n' +
+        ' A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally\n' +
+        'bred for hunting.'
+    },
   ];
 
   constructor(private newsService: NewsService) { }
@@ -28,5 +56,7 @@ export class NewsStreamComponent implements OnInit {
       this.newsItems.push(newsItem);
     })
   }
+
+
 
 }
